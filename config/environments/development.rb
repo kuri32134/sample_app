2f87b9feb86ac6915a42f515b7063c303540b0bb
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-03-27 02:44:06 +0000

    [Add]モデルとコントローラ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "cab618b44d044939a22ff89ee45abd0a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
